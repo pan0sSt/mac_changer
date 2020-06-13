46278f680ae6e2584f67618a47fa3c018d783441
pan0sSt <p.stavrinakis@gmail.com>
2020-06-13 13:44:39 -0400

    Handle user arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess # call() function for shell commands
+import optparse   # get values as arguments
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+# function that handles the user arguments
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="mac_addr", help="New MAC Address")
+    (options, arguments) = parser.parse_args()  # handles arguments keys and values
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.mac_addr:
+        parser.error("[-] Please specify a new MAC Address, use --help for more info.")
+    return options
+
+# function that changes the MAC Address of selected interface
+def change_mac(interface, mac_addr):
+    print("[+] Changing MAC Address for " + interface + " to " + mac_addr)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_addr])
+    subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface, options.mac_addr)
